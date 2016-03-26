e84d4ce86b9178a6c82fa8c6f7966fad12629f3d
M <m-coding@users.noreply.github.com>
2016-03-25 18:07:07 -0700

    Rename TabView to TabNav

--- public/src/js/views/tabView.js
+++ public/src/js/views/tabView.js
@@ -1,13 +1,13 @@
 /** Tab navigation
  * @namespace  nt.Views
- * @class nt.Views.TabView
+ * @class nt.Views.TabNav
  * @memberof! <global>
  * @extends Backbone.View */
-nt.Views.TabView = Backbone.View.extend(/** @lends nt.Views.TabView# */{
+nt.Views.TabNav = Backbone.View.extend(/** @lends nt.Views.TabNav# */{
 
     /** Enable bootstrap tabs
      * @function  initialize
-     * @memberof  nt.Views.TabView */
+     * @memberof  nt.Views.TabNav */
     initialize: function() {
         $('#app-tabs a').click(function (e) {
             e.preventDefault();
