be555d759726ff4f74f0545f26d30c0c335898f7
M <m-coding@users.noreply.github.com>
2016-03-25 18:31:34 -0700

    Add encodeURIComponent to searchPhrase

--- public/src/js/collections/foodSearch.js
+++ public/src/js/collections/foodSearch.js
@@ -10,45 +10,12 @@ nt.Collections.FoodSearch = Backbone.Collection.extend(/** @lends nt.Collections
 
     /** Nutritionix API /search will return an array of matching foods */
     url: function() {
-        return 'https://api.nutritionix.com/v1_1/search/' + this.searchPhrase;
-    },
-
-    /** Override fetch to pass in search phrase and parameters */
-    fetch1: function(options, searchPhrase) {
-        var _url = this.url;
-
-        if(searchPhrase) {
-            // credit: https://github.com/jashkenas/backbone/issues/93
-            this.url = this.url + searchPhrase;
-        }
-
-        this.fetch(options);
-
-        this.url = _url;
-    },
-
-    fetch2: function(options, searchPhrase, parameters) {
-        var _url = this.url;
-
-        if(searchPhrase && parameters) {
-            // credit: https://github.com/jashkenas/backbone/issues/93
-            this.url = this.url + searchPhrase + '?' + $.param(parameters);
-        }
-
-        this.fetch(options);
-
-        this.url = _url;
+        return 'https://api.nutritionix.com/v1_1/search/' + encodeURIComponent(this.searchPhrase);
     },
 
     /** Override parse to return only "hits" from the response */
     parse: function(response) {
         return response.hits;
-    },
-
-    error: function(model, response, options) {
-        console.log(model);
-        console.log(response);
-        console.log(options);
     },
 
     /** This generates the next order number for new items.
