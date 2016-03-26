a73655c01e25a06ad135fc03cd27c7db775fa11a
M <m-coding@users.noreply.github.com>
2016-03-25 18:05:16 -0700

    Update names

--- public/src/js/router.js
+++ public/src/js/router.js
@@ -7,25 +7,27 @@
 // The # tells JSDoc that the items lent to nt.Router belong to an instance of the class.
 nt.Router = Backbone.Router.extend(/** @lends nt.Router# */{
     routes:{
+        'search/:query': 'search'
     },
 
+    search: function(query) {
+        console.log('Food search containing the word: ' + query);
+    },
 
     /** Instantiate Models, Collections, and Views
      * @function initialize
      * @memberof nt.Router */
     initialize: function() {
-        nt.Models.nutrition = new nt.Models.Nutrition();
-
+        nt.Models.nutrition    = new nt.Models.Nutrition();
         nt.Collections.results = new nt.Collections.FoodSearch();
         nt.Collections.recipes = new nt.Collections.FoodRecipes();
         nt.Collections.tracker = new nt.Collections.FoodTracker();
-
-        nt.Views.tabs = new nt.Views.TabView();
-        nt.Views.start = new nt.Views.StartView();
-        nt.Views.search = new nt.Views.SearchView();
-        nt.Views.recipe = new nt.Views.RecipeView();
-        nt.Views.nutrition = new nt.Views.NutritionView();
-        nt.Views.tracker = new nt.Views.TrackerView();
-      //nt.Views.food = new nt.Views.FoodView();
+        nt.Views.tabs          = new nt.Views.TabNav();
+        nt.Views.start         = new nt.Views.Start();
+        nt.Views.search        = new nt.Views.Search({collection: nt.Collections.results});
+        nt.Views.recipe        = new nt.Views.Recipe({collection: nt.Collections.recipes});
+        nt.Views.nutrition     = new nt.Views.Nutrition({model: nt.Models.nutrition});
+        nt.Views.tracker       = new nt.Views.Tracker({collection: nt.Collections.tracker});
+      //nt.Views.food          = new nt.Views.FoodView();
     }
 });
