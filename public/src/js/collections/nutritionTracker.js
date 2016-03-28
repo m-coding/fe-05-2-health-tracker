20b1ed5842f22784063b8ba0bf842f669ae59d2a
M <m-coding@users.noreply.github.com>
2016-03-28 00:25:08 -0700

    Rename js files to match models used
    MODEL
    * nutrition.js
    
    COLLECTION
    * nutritionTracker.js
    
    VIEW
    * nutritionTrackerView.js

---
+++ public/src/js/collections/nutritionTracker.js
@@ -0,0 +1,29 @@
+/** Collection of foods being tracked
+ * @namespace  nt.Collections
+ * @class nt.Collections.NutritionTracker
+ * @memberof! <global>
+ * @extends Backbone.Collection */
+nt.Collections.NutritionTracker = Backbone.Collection.extend(/** @lends nt.Collections.NutritionTracker# */{
+
+    /** Reference to this collection's model. */
+    model: nt.Models.Food,
+
+    /** Save all of the food items in localStorage */
+    localStorage: new Backbone.LocalStorage('nutrition-test1'),
+
+    // This generates the next order number for new items.
+    nextOrder: function() {
+        console.log('NutritionTracker nextOrder()');
+        if ( !this.length ) {
+            return 1;
+        }
+            return this.last().get('sortOrder') + 1;
+    },
+
+    // Each food item is sorted by its original insertion order.
+    comparator: function( food ) {
+            console.log('NutritionTracker comparator()');
+            return food.get('sortOrder');
+    }
+
+});
