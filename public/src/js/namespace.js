ef686de4bbf34cd964e7f9948a784e5e10642946
M <m-coding@users.noreply.github.com>
2016-03-31 18:36:54 -0700

    Add namespace.js

---
+++ public/src/js/namespace.js
@@ -0,0 +1,11 @@
+/**! Nutrition Tracker App
+ * @namespace {object} nt */
+
+// Namespace everything in an `nt` object to prevent polluting the global namespace.
+var nt = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Router: {},
+    Plugin: {}
+};
