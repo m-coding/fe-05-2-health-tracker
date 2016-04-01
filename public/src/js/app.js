01eee5407e2aa63e52ce853692436d8ab11c09f7
M <m-coding@users.noreply.github.com>
2016-03-31 18:37:34 -0700

    Remove namespace code from app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,23 @@
-/**! Nutrition Tracker App
- * @namespace {object} nt */
-
-// Namespace everything in an `nt` object to prevent polluting the global namespace.
-var nt = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Router: {}
-};
-
-// Instantiate the Router when the DOM is ready.
+// Setup Nutrition Tracker app when DOM is ready
 $(document).ready(function() {
+    // Instantiate Models and Collections
+    nt.Models.nutrition    = new nt.Models.Nutrition();
+    nt.Collections.suggest = new nt.Collections.AutocompleteSearch();
+    nt.Collections.results = new nt.Collections.FoodSearch();
+    nt.Collections.recipes = new nt.Collections.RecipeSearch();
+    nt.Collections.tracker = new nt.Collections.NutritionTracker();
+
+    // Instantiate Views
+    nt.Views.tabs        = new nt.Views.TabNav();
+    nt.Views.start       = new nt.Views.Start();
+    nt.Views.search      = new nt.Views.Search({ collection: nt.Collections.results });
+    nt.Views.recipe      = new nt.Views.Recipe({ collection: nt.Collections.recipes });
+    nt.Views.nutrition   = new nt.Views.Nutrition({ model: nt.Models.nutrition });
+    nt.Views.tracker     = new nt.Views.Tracker({ collection: nt.Collections.tracker });
+
+    // Instantiate the Router
     nt.Router.Instance = new nt.Router();
     Backbone.history.start();
+    nt.Router.Instance.navigate('start');
 });
+
