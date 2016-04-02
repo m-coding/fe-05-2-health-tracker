afce236dc97258571a95bba8f0e531d043a9943f
M <m-coding@users.noreply.github.com>
2016-04-02 02:05:35 -0700

    Update search view

--- public/src/js/views/foodSearchView.js
+++ public/src/js/views/foodSearchView.js
@@ -12,7 +12,7 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
     itemTemplate: Handlebars.compile( $('#item-template').html() ),
 
     events: {
-        'submit #search-suggest': 'searchFood'
+        //'submit #search-suggest': 'searchFood'
     },
 
     /** Setup `this` context, DOM references, and listeners */
@@ -22,10 +22,15 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
         this.$searchTop = $('#search-top');
         this.$searchResults = $('#search-results');
         this.$searchFood = $('#search-food');
+        this.$preload = $('.typeahead-preload');
+        this.$dropmenu = $('#search-suggest .dropdown-menu');
 
         // Run the search if the user selects an option from the autocomplete list
         this.listenTo(nt.Plugin.Instance, 'selected', this.searchFood);
+        // Run the search if the enter key is pressed
+        // this.listenTo(nt.Plugin.Instance, 'enterkey', this.searchFood);
 
+        // Show/Hide preloader message
         this.listenTo(nt.Collections.suggest, 'fetch', this.showPreloader);
         this.listenTo(nt.Collections.suggest, 'sync', this.hidePreloader);
     }, // initialize
@@ -45,21 +50,13 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
     showPreloader: function() {
         var msg = 'Loading...';
         var html = '<li class="typeahead-preload"><a>' + msg + '</a></li>';
-        var elem = $('#search-suggest .typeahed-preload');
-        $('.typeahead-preload').remove();
-        $('#search-suggest .dropdown-menu').show().prepend(html);
+        this.$preload.remove();
+        this.$dropmenu.show().prepend(html);
     }, // showPreloader
 
     hidePreloader: function() {
-        $('.typeahead-preload').remove();
+        this.$preload.remove();
     }, // hidePreloader
-
-    autoSelected: function(model) {
-        console.log('autoSelected');
-        console.log('model:');
-        console.dir(model);
-        console.log('The user selected:', model.get('text'));
-    }, // autoSelected
 
     searchSuccess: function(collection, response) {
         this.render();
@@ -71,10 +68,7 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
     }, // searchError
 
     searchFood: function(e) {
-        console.log('searchFood()');
         var query = this.$searchFood.val().trim();
-
-        console.log('e.which: ' + e.which);
 
         // Nutritionix API v.1.1 Field Reference
         // https://docs.google.com/spreadsheets/d/1jZSa039OfpQOiRzaS980nPKCvVe2TRKRPZk7ZbaH7kE/edit#gid=0
@@ -90,11 +84,6 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
             'appId': '53242d79',
             'appKey': '82289438a16ec7b92cdcf5ad054159c4'
         };
-
-        // Close the autocomplete drop menu if ENTER key pressed
-        if(e.which === 13) {
-            console.log('ENTER KEY PRESSED');
-        }
 
         if (query.length > 0 && query !== this.prevQuery) {
             // Clear out all the models in the collection
