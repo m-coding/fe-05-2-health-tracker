b5f95a5446039cb5f9193dcea8495e24c045cc50
M <m-coding@users.noreply.github.com>
2016-04-01 22:15:53 -0700

    Group plugin variables together

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -7,13 +7,20 @@ $(document).ready(function() {
     nt.Collections.recipes = new nt.Collections.RecipeSearch();
     nt.Collections.tracker = new nt.Collections.NutritionTracker();
 
+    // Instantiate the autocomplete plugin
+    nt.Plugin.Typeahead = Backbone.Typeahead.extend({
+        template: '<input id="search-food" type="search" class="form-control" placeholder="What did you eat?"><ul class="dropdown-menu"></ul>'
+    });
+    nt.Plugin.Instance = new nt.Plugin.Typeahead({ collection: nt.Collections.suggest, key: 'text' });
+    nt.Plugin.Instance.setElement('#search-suggest').render();
+
     // Instantiate Views
-    nt.Views.tabs        = new nt.Views.TabNav();
-    nt.Views.start       = new nt.Views.Start();
-    nt.Views.search      = new nt.Views.Search({ collection: nt.Collections.results });
-    nt.Views.recipe      = new nt.Views.Recipe({ collection: nt.Collections.recipes });
-    nt.Views.nutrition   = new nt.Views.Nutrition({ model: nt.Models.nutrition });
-    nt.Views.tracker     = new nt.Views.Tracker({ collection: nt.Collections.tracker });
+    nt.Views.start     = new nt.Views.Start();
+    nt.Views.tabs      = new nt.Views.TabNav();
+    nt.Views.search    = new nt.Views.Search({ collection: nt.Collections.results });
+    nt.Views.recipe    = new nt.Views.Recipe({ collection: nt.Collections.recipes });
+    nt.Views.nutrition = new nt.Views.Nutrition({ model: nt.Models.nutrition });
+    nt.Views.tracker   = new nt.Views.Tracker({ collection: nt.Collections.tracker });
 
     // Instantiate the Router
     nt.Router.Instance = new nt.Router();
