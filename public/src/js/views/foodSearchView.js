02c89483ad6ae339799721452e9ca0751fcb1387
M <m-coding@users.noreply.github.com>
2016-04-01 22:17:26 -0700

    Autocomplete updates on fetch request instead of call to view

--- public/src/js/views/foodSearchView.js
+++ public/src/js/views/foodSearchView.js
@@ -5,16 +5,14 @@
  * @extends Backbone.View */
 nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
 
-    el: '#search',
-
-    //router: nt.Router,
+    el: '#search-top',
 
     prevQuery: '',
 
     itemTemplate: Handlebars.compile( $('#item-template').html() ),
 
     events: {
-        'keyup #search-food': 'searchFood'
+        'submit #search-suggest': 'searchFood'
     },
 
     /** Setup `this` context, DOM references, and listeners */
@@ -24,7 +22,13 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
         this.$searchTop = $('#search-top');
         this.$searchResults = $('#search-results');
         this.$searchFood = $('#search-food');
-    },
+
+        // Run the search if the user selects an option from the autocomplete list
+        this.listenTo(nt.Plugin.Instance, 'selected', this.searchFood);
+
+        this.listenTo(nt.Collections.suggest, 'fetch', this.showPreloader);
+        this.listenTo(nt.Collections.suggest, 'sync', this.hidePreloader);
+    }, // initialize
 
     /** Render results */
     render: function() {
@@ -36,7 +40,26 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
         this.$searchResults.append( this.itemTemplate({ items: this.collection.toJSON() }) );
 
         return this;
-    },
+    }, // render
+
+    showPreloader: function() {
+        var msg = 'Loading...';
+        var html = '<li class="typeahead-preload"><a>' + msg + '</a></li>';
+        var elem = $('#search-suggest .typeahed-preload');
+        $('.typeahead-preload').remove();
+        $('#search-suggest .dropdown-menu').show().prepend(html);
+    }, // showPreloader
+
+    hidePreloader: function() {
+        $('.typeahead-preload').remove();
+    }, // hidePreloader
+
+    autoSelected: function(model) {
+        console.log('autoSelected');
+        console.log('model:');
+        console.dir(model);
+        console.log('The user selected:', model.get('text'));
+    }, // autoSelected
 
     searchSuccess: function(collection, response) {
         this.render();
@@ -45,12 +68,13 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
     searchError: function(collection, errorResponse) {
         console.log('nt.Collections.results.fetch ERROR: ' + errorResponse);
         console.log('NUTRITIONIX REQUEST FAILED');
-    },
+    }, // searchError
 
     searchFood: function(e) {
-        if(e.which === 13) return false; // prevent ENTER key submit
-
+        console.log('searchFood()');
         var query = this.$searchFood.val().trim();
+
+        console.log('e.which: ' + e.which);
 
         // Nutritionix API v.1.1 Field Reference
         // https://docs.google.com/spreadsheets/d/1jZSa039OfpQOiRzaS980nPKCvVe2TRKRPZk7ZbaH7kE/edit#gid=0
@@ -66,6 +90,11 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
             'appId': '53242d79',
             'appKey': '82289438a16ec7b92cdcf5ad054159c4'
         };
+
+        // Close the autocomplete drop menu if ENTER key pressed
+        if(e.which === 13) {
+            console.log('ENTER KEY PRESSED');
+        }
 
         if (query.length > 0 && query !== this.prevQuery) {
             // Clear out all the models in the collection
@@ -87,6 +116,9 @@ nt.Views.Search = Backbone.View.extend(/** @lends nt.Views.Search# */{
 
         // Update url
         nt.Router.Instance.goto('search/' + query);
+
+        // Prevent form submission
+        return false;
 
     } // searchFood
 
