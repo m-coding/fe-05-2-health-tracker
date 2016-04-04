9ba46409ad6661a1a448b262ab07c95e388b0894
M <m-coding@users.noreply.github.com>
2016-04-03 22:59:11 -0700

    Update search collection

--- public/src/js/collections/foodSearch.js
+++ public/src/js/collections/foodSearch.js
@@ -13,12 +13,12 @@ nt.Collections.FoodSearch = Backbone.Collection.extend(/** @lends nt.Collections
     /** Nutritionix API /search will return an array of matching foods */
     url: function() {
         return 'https://api.nutritionix.com/v1_1/search/' + this.searchPhrase;
-    },
+    }, // url
 
     /** Override parse to return only "hits" from the response */
     parse: function(response) {
         return response.hits;
-    },
+    }, // parse
 
     /** This generates the next order number for new items.
     * @returns {number} Next order number */
@@ -27,11 +27,13 @@ nt.Collections.FoodSearch = Backbone.Collection.extend(/** @lends nt.Collections
             return 1;
         }
             return this.last().get('sortOrder') + 1;
-    },
+
+    }, // nextOrder
 
     /**  Each food item is sorted by its original insertion order.
-    * @returns {number} Sor order */
+    * @returns {number} Sort order */
     comparator: function( food ) {
             return food.get('sortOrder');
-        }
+    } // comparator
+
 });
