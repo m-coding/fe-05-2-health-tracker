eb8c9de5a135e498cf2db8a097dc8e468917a538
M <m-coding@users.noreply.github.com>
2016-04-03 22:52:23 -0700

    Use default icon if recipe icon not available

--- public/src/js/models/recipe.js
+++ public/src/js/models/recipe.js
@@ -21,13 +21,18 @@ nt.Models.Recipe = Backbone.Model.extend(/** @lends nt.Models.Recipe# */{
     parse: function(data) {
         var prefix = 'http://www.edamam.com/http/';
         var index  = data.recipe.uri.indexOf('_') + 1;
+        var icon = 'images/icon.png';
+
+        if(data.recipe.sourceIcon)
+            icon = data.recipe.sourceIcon.replace('http://', prefix);
+
         var recipe = {};
             recipe.id             = data.recipe.uri.slice(index);
             recipe.recipeImage    = data.recipe.image;
             recipe.recipeTitle    = data.recipe.label;
             recipe.numCalories    = parseInt(data.recipe.calories / data.recipe.yield, 10);
             recipe.numIngredients = data.recipe.ingredientLines.length;
-            recipe.siteIcon       = data.recipe.sourceIcon.replace('http://', prefix);
+            recipe.siteIcon       = icon;
             recipe.siteTitle      = data.recipe.source;
             recipe.siteLink       = data.recipe.shareAs;
         return recipe;
