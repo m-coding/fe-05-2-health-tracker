767cc73ff2b30a26d99802118626b22bf4ed9c2f
M <m-coding@users.noreply.github.com>
2016-04-04 18:36:31 -0700

    Change 'an' to 'a' in comment

--- public/src/js/namespace.js
+++ public/src/js/namespace.js
@@ -1,7 +1,7 @@
 /**! Nutrition Tracker App
  * @namespace {object} nt */
 
-// Namespace everything in an `nt` object to prevent polluting the global namespace.
+// Namespace everything in a `nt` object to prevent polluting the global namespace.
 var nt = {
     Models: {},
     Collections: {},
