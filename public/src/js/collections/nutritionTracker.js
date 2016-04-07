7906cb8cdbd037840ac926c3ebab9fd118dab394
M <m-coding@users.noreply.github.com>
2016-04-07 01:05:10 -0700

    Add getModelsByDate function and set global tracker date option

--- public/src/js/collections/nutritionTracker.js
+++ public/src/js/collections/nutritionTracker.js
@@ -11,6 +11,11 @@ nt.Collections.NutritionTracker = Backbone.Collection.extend(/** @lends nt.Colle
     /** Save all of the food items in localStorage. */
     localStorage: new Backbone.LocalStorage('nutrition-tracker'),
 
+    /** Set default tracker date for today */
+    initialize: function() {
+        nt.Option.trackerDate = moment(new Date()).format('YYYY-MM-DD');
+    }, // initialize
+
     /** This generates the next order number for new items. */
     nextOrder: function() {
         if ( !this.length ) {
@@ -23,7 +28,18 @@ nt.Collections.NutritionTracker = Backbone.Collection.extend(/** @lends nt.Colle
     /** Each food item is sorted by its original insertion order. */
     comparator: function( food ) {
             return food.get('sortOrder');
+    }, // comparator
 
-    } // comparator
+    /** Returns a group of models with the same date */
+    getModelsByDate: function() {
+        // Filter the models by date
+        var groupArray = this.where({ trackDate: nt.Option.trackerDate });
+
+        // Convert the array back into a Collection
+        var groupCollection = new nt.Collections.NutritionTracker(groupArray);
+
+        return groupCollection;
+
+    } // getModelsByDate
 
 });
